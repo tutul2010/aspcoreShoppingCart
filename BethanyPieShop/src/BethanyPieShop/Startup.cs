be5ad3ab69274b3c69c95782c7e4e4aae59e9314
User <User@DESKTOP-IP6GPOK>
2017-03-25 13:06:34 +0530

    Push commit to local and Remote for feature1.3

--- BethanyPieShop/src/BethanyPieShop/Startup.cs
+++ BethanyPieShop/src/BethanyPieShop/Startup.cs
@@ -29,7 +29,7 @@ namespace BethanyPieShop
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-           
+           //code changes for feature1.3
             //register custome AppDbContext cls with conn-str in appsettings.json file
             services.AddDbContext<AppDbContext>(options =>
                                         options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
